dd762fe41f8ed11959a71c4ff9c1b9c324c02547
Sam Krause <samuelnkrause@gmail.com>
2016-03-10 16:53:07 -0500

    completed

--- railsujs/app/controllers/posts_controller.rb
+++ railsujs/app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:message)
+  end
+
 end
