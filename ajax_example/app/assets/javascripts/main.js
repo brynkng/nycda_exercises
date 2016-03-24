e0f440914ce800b6bbf2df4611496baede648154
Sam Krause <samuelnkrause@gmail.com>
2016-03-24 15:06:44 -0400

    finished log in

--- ajax_example/app/assets/javascripts/main.js
+++ ajax_example/app/assets/javascripts/main.js
@@ -10,6 +10,7 @@ $(function() {
 
   function setup_signup() {
     $('#signup-show').click(function() {
+      $('.panel').removeClass('show');
       $('#signup').addClass('show')
     })
 
@@ -19,8 +20,8 @@ $(function() {
 
     $('#signup-button').click(function() {
       var data = {
-        email: $('#email').val(),
-        password: $('#password').val(),
+        email: $('#signup .email').val(),
+        password: $('#signup .password').val(),
         password_confirmation: $('#password_confirmation').val(),
         first_name: $('#first_name').val(),
         last_name: $('#last_name').val()
@@ -43,13 +44,31 @@ $(function() {
 
   function setup_login() {
     $('#login-show').click(function() {
+      $('.panel').removeClass('show');
       $('#login').addClass('show')
     })
 
     $('#login .close').click(function() {
       $('#login').removeClass('show')
     })
+
+    $('#login-button').click(function() {
+      var data = {
+        email: $('#login .email').val(),
+        password: $('#login .password').val(),
+      }
+
+        $.post(
+          '/sessions/login',
+          data,
+          function(response) {
+            if (response.success) {
+              $('#container').html('Successfully signed logged in!')
+            } else {
+              $('#container').html('Either the email or password is Wrong!')
+            }
+          }
+        )
+    })
   }
-
-
 })
